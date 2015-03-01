9bbb2b36814dcf7cf3bfbcccf467a0f4ba50968e
realityone <realityone@realityonedeMacBook-Pro.local>
2015-03-01 22:53:45 +0800

    fix bug

--- CetTicket.py
+++ CetTicket.py
@@ -28,8 +28,8 @@ class CetCrypter(object):
     ticket_number_key = '(YesuNRY'
     request_data_key = '?!btwNP^'
 
-    ENCRYPT = c_int(1)
-    DECRYPT = c_int(0)
+    DECRYPT = 0
+    ENCRYPT = 1
 
     def __init__(self, libcrypto_path=None):
         if not libcrypto_path:
@@ -65,19 +65,19 @@ class CetCrypter(object):
 
     def decrypt_ticket_number(self, ciphertext):
         ciphertext = ciphertext[2:]
-        return self.process_data(ciphertext, self.ticket_number_key, 0)
+        return self.process_data(ciphertext, self.ticket_number_key, is_enc=self.DECRYPT)
 
     def encrypt_ticket_number(self, ticket_number):
-        ciphertext = self.process_data(
-            ticket_number, self.ticket_number_key, 1)
+        ciphertext = self.process_data(ticket_number,
+                                       self.ticket_number_key, is_enc=self.ENCRYPT)
         ciphertext = '\x35\x2c' + ciphertext
         return ciphertext
 
     def decrypt_request_data(self, ciphertext):
-        return self.process_data(ciphertext, self.request_data_key, 0)
+        return self.process_data(ciphertext, self.request_data_key, is_enc=self.DECRYPT)
 
-    def encrypt_request_data(self, plaintext):
-        return self.process_data(plaintext, self.request_data_key, 1)
+    def encrypt_request_data(self, request_data):
+        return self.process_data(request_data, self.request_data_key, is_enc=self.ENCRYPT)
 
 
 class CetTicket(object):
@@ -90,33 +90,37 @@ class CetTicket(object):
 
     search_url = CetConfig.SEARCH_URL
 
-    def __init__(self):
-        self.crypter = CetCrypter()
+    CET4 = 1
+    CET6 = 2
 
-    def find_ticket_number(self, provice, school, name, examroom='', cet_type=1):
+    @classmethod
+    def find_ticket_number(cls, provice, school, name, examroom='', cet_type=1):
         """
             You can read the `school.json` file to check if your school is supported.
             cet_type:
                     1 ==> cet4
                     2 ==> cet6
         """
+        crypter = CetCrypter()
+
         provice_id = CetConfig.PROVICE[provice]
         param_data = b'type=%d&provice=%d&school=%s&name=%s&examroom=%s' % (cet_type,
                                                                             provice_id,
                                                                             school, name, examroom)
 
         param_data = param_data.decode('utf-8').encode('gb2312')
-        encrypted_data = self.crypter.encrypt_request_data(param_data)
+        encrypted_data = crypter.encrypt_request_data(param_data)
 
-        resp = requests.post(url=self.search_url, data=encrypted_data)
+        resp = requests.post(url=cls.search_url, data=encrypted_data)
 
-        ticket_number = self.crypter.decrypt_ticket_number(resp.content)
+        ticket_number = crypter.decrypt_ticket_number(resp.content)
         if ticket_number == '':
             raise Exception('Cannot find ticket number.')
 
         return ticket_number
 
-    def get_score(self, ticket_number, name):
+    @classmethod
+    def get_score(cls, ticket_number, name):
         if isinstance(name, unicode):
             name = name.encode('gb2312')
         else:
@@ -144,6 +148,5 @@ class CetTicket(object):
         return score
 
 if __name__ == '__main__':
-    ct = CetTicket()
-    print ct.find_ticket_number(b'浙江', b'浙江海洋学院', b'XXX', cet_type=1)
-    print ct.get_score('330400XXXXXXXXX', b'XXX')
+    print CetTicket.find_ticket_number(b'浙江', b'浙江海洋学院', b'XXX', cet_type=CetTicket.CET4)
+    print CetTicket.get_score('330400XXXXXXXX', b'XXX')
